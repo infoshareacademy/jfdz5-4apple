d294366fcae0d080fb7ce044e9e8667e32491df9
Piotr Stefanowicz <stefan8357@gmail.com>
2017-06-20 03:00:25 +0200

    FZ54A-28-multi-language/ adding form translation

--- JavaScript/multi-language/changingLanguage.js
+++ JavaScript/multi-language/changingLanguage.js
@@ -1,6 +1,9 @@
 (function () {
 
     var isPolish;
+    var elNamePlaceholder = document.getElementById('form-name');
+    var elButton = document.getElementById('sign-in');
+    var elSubmitAlert = document.getElementsByClassName('newsletter-form');
 
     var elLanguage = document.getElementById('language');
     elLanguage.addEventListener('click', displayLanguage);
@@ -18,10 +21,17 @@
 
         if (isPolish === true) {
             elLanguage.setAttribute('class', 'language flag-en');
+            elNamePlaceholder.setAttribute('placeholder', 'Imię');
+            elButton.setAttribute('value', 'zapisz się!');
+            elSubmitAlert[0].setAttribute('onsubmit', "alert('Uważnie obserwuj swoją pocztę. W nieodległej przyszłości otrzyamasz wiadomość o otwarciu naszego serwisu.')");
             isPolish = false;
+
         }
         else {
             elLanguage.setAttribute('class', 'language flag-pl');
+            elNamePlaceholder.setAttribute('placeholder', 'Name');
+            elButton.setAttribute('value', 'sign up!');
+            elSubmitAlert[0].setAttribute('onsubmit',"alert('Carefully watch your email. In the near future you will receive message about opening our service.')");
             isPolish = true;
         }
     }
