68b5a3262fca29c19bf672cd5d9cbbe310ab1bb2
Piotr Stefanowicz <stefan8357@gmail.com>
2017-06-23 17:21:20 +0200

    FZ54A-21-cookies-info/ add info closures

--- js/cookies-info/cookies-info-display.js
+++ js/cookies-info/cookies-info-display.js
@@ -1,3 +1,17 @@
-/**
- * Created by Piotrek on 23.06.2017.
- */
+(function(){
+
+    var elCookiesInfoContainer = document.getElementsByClassName('cookies-info--display')[0];
+    var elCookiesInfoClosure = document.getElementsByClassName('cookies-info__closure')[0];
+    var elCookiesInfoAccept = document.getElementsByClassName('accept-cookies__button')[0];
+
+    elCookiesInfoAccept.addEventListener('click', accept );
+    elCookiesInfoClosure.addEventListener('click', close );
+
+    function close(){
+        elCookiesInfoContainer.setAttribute('class', 'cookies-info--hide');
+    }
+    function accept(){
+        elCookiesInfoContainer.setAttribute('class', 'cookies-info--hide');
+    }
+
+})();
