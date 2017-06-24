e59bc5afa191e43a6a3277c802e7dae18826be18
Piotr Stefanowicz <stefan8357@gmail.com>
2017-06-24 23:48:29 +0200

    FZ54A-27-back-to-the-top/ deleting jquery files

--- js/back-to-the-top/smoothScroll.js
+++ js/back-to-the-top/smoothScroll.js
@@ -1,5 +1,14 @@
-jQuery(function ($) {
-    $('#button').click(function () {
-        $.scrollTo($('#home'), 500);
-    });
-});
+(function () {
+    var elArrow = document.getElementById('button');
+
+    elArrow.addEventListener('click', scroll);
+
+    function scroll() {
+        var scrolling = setInterval(function () {
+            window.scrollTo(0, ((windowPosition - (0.15 * windowPosition))));
+            if (windowPosition === 0) {
+                clearInterval(scrolling);
+            }
+        }, 1)
+    }
+})();
