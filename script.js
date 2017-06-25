8ae17e01682a2ad536db9e084f74be752e7cc948
Aleksander <Konfucjussz29@gmail.com>
2017-06-25 20:07:56 +0200

    add scroll height window

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ $(function () {
     var second = $('.animation-first');
 
     $(window).scroll(function () {
-        if (parseInt($(window).scrollTop()) > picturesPositionTop) {
+        if (parseInt($(window).scrollTop()+$(window).height()-400) > (picturesPositionTop)) {
             if (now.hasClass('animation')) {
                 now.removeClass('animation').addClass('animation-team');
                 second.removeClass('animation-first').addClass('animation-second');
             }
         }
     });
+    console.log(picturesPositionTop);
 });
