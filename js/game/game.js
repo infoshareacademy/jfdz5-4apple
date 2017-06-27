efcf406e0edbf26102cb7a2a3607b4e8be315868
Piotr Stefanowicz <stefan8357@gmail.com>
2017-06-27 12:00:35 +0200

    FZ54A-22-game-DEVELOP/ increasing size of the character

--- js/game/game.js
+++ js/game/game.js
@@ -2,20 +2,20 @@ var position = 0;
 
 $(window).keydown(function (e) {
     $character = $('.character');
-    if (position <=(-275)){
-        position +=25;
+    if (position <=(-255)){
+        position +=15;
         $character.css({'transform': 'translateX(' + position + 'px)'});
     }
-    else if (position >=275){
-        position -=25;
+    else if (position >=255){
+        position -=15;
         $character.css({'transform': 'translateX(' + position + 'px)'});
     }
     else if (e.keyCode === 37) {
-        position -= 25;
+        position -= 15;
         $character.css({'transform': 'translateX(' + position + 'px)'});
     }
     else if (e.keyCode === 39) {
-        position += 25;
+        position += 15;
         $character.css({'transform': 'translateX(' + position + 'px)'});
     }
 
