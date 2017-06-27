5d015d332ebeed0cb2236d53456aeb20587211b3
Piotr Stefanowicz <stefan8357@gmail.com>
2017-06-27 13:41:43 +0200

    FZ54A-22-game-DEVELOP/ making character turn ledt and right

--- js/game/game.js
+++ js/game/game.js
@@ -2,21 +2,29 @@ var position = 0;
 
 $(window).keydown(function (e) {
     $character = $('.character');
-    if (position <=(-255)){
-        position +=15;
+    if (position <= (-255)) {
+        position += 15;
         $character.css({'transform': 'translateX(' + position + 'px)'});
     }
-    else if (position >=255){
-        position -=15;
+    else if (position >= 255) {
+        position -= 15;
         $character.css({'transform': 'translateX(' + position + 'px)'});
     }
     else if (e.keyCode === 37) {
         position -= 15;
-        $character.css({'transform': 'translateX(' + position + 'px)'});
+        $character.css({
+            'transform': 'translateX(' + position + 'px)',
+            'background': 'url("ludzik-z-workiem-lewo.png")',
+            'background-size': 'cover'
+        });
     }
     else if (e.keyCode === 39) {
         position += 15;
-        $character.css({'transform': 'translateX(' + position + 'px)'});
+        $character.css({
+            'transform': 'translateX(' + position + 'px)',
+            'background': 'url("ludzik-z-workiem.png")',
+            'background-size': 'cover'
+        });
     }
 
 });
