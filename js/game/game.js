1fb75dcfc52be708b48f086928390c847f60e403
Piotr Stefanowicz <stefan8357@gmail.com>
2017-06-29 15:10:00 +0200

    FZ54A-22-game-DEVELOP/ adding background img

--- js/game/game.js
+++ js/game/game.js
@@ -35,7 +35,7 @@ var cardboardBoxFall = setInterval(function () {
     $('.cardboard-box').css({
         'transform': 'translateY(' + height + 'px)'
     });
-    if (height >= 680) {
+    if (height >= 560) {
         clearInterval(cardboardBoxFall);
     }
 }, 100);
