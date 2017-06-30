f42a533672c006f31ec1e8cfe5be93f13ca98238
Piotr Stefanowicz <stefan8357@gmail.com>
2017-06-30 13:03:56 +0200

    FZ54A-22-game-MENU/ added menu toggle animation

--- js/game/game-menu.js
+++ js/game/game-menu.js
@@ -1,8 +1,10 @@
 (function () {
 
     $('.game-menu--item:nth-child(1)').click(function () {
-        $('.game-menu').hide();
-        $('.game').show();
-    })
+        $('.game-menu').animate({height: 'toggle'});
+        setTimeout(function () {
+            $('.game').show();
+        }, 500);
+    });
 
 }());
