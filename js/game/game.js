962103443f309470e2a1b661a6d766e48eacfac3
Piotr Stefanowicz <stefan8357@gmail.com>
2017-06-30 03:20:10 +0200

    FZ54A-22-game-MENU/ adding IIFE

--- js/game/game.js
+++ js/game/game.js
@@ -1,41 +1,44 @@
-var position = 0;
-var height = 0;
+(function () {
 
-$(window).keydown(function (e) {
-    $character = $('.character');
-    if (position <= (-255)) {
-        position += 15;
-        $character.css({'transform': 'translateX(' + position + 'px)'});
-    }
-    else if (position >= 255) {
-        position -= 15;
-        $character.css({'transform': 'translateX(' + position + 'px)'});
-    }
-    else if (e.keyCode === 37) {
-        position -= 15;
-        $character.css({
-            'transform': 'translateX(' + position + 'px)',
-            'background': 'url("img/ludzik-z-workiem-lewo.png")',
-            'background-size': 'cover'
-        });
-    }
-    else if (e.keyCode === 39) {
-        position += 15;
-        $character.css({
-            'transform': 'translateX(' + position + 'px)',
-            'background': 'url("img/ludzik-z-workiem.png")',
-            'background-size': 'cover'
-        });
-    }
+    position = 0;
+    var height = 0;
 
-});
+    $(window).keydown(function (e) {
+        $character = $('.character');
+        if (position <= (-255)) {
+            position += 15;
+            $character.css({'transform': 'translateX(' + position + 'px)'});
+        }
+        else if (position >= 255) {
+            position -= 15;
+            $character.css({'transform': 'translateX(' + position + 'px)'});
+        }
+        else if (e.keyCode === 37) {
+            position -= 15;
+            $character.css({
+                'transform': 'translateX(' + position + 'px)',
+                'background': 'url("img/ludzik-z-workiem-lewo.png")',
+                'background-size': 'cover'
+            });
+        }
+        else if (e.keyCode === 39) {
+            position += 15;
+            $character.css({
+                'transform': 'translateX(' + position + 'px)',
+                'background': 'url("img/ludzik-z-workiem.png")',
+                'background-size': 'cover'
+            });
+        }
 
-var cardboardBoxFall = setInterval(function () {
-    height += 10;
-    $('.cardboard-box').css({
-        'transform': 'translateY(' + height + 'px)'
     });
-    if (height >= 480) {
-        clearInterval(cardboardBoxFall);
-    }
-}, 100);
+
+    var cardboardBoxFall = setInterval(function () {
+        height += 10;
+        $('.cardboard-box').css({
+            'transform': 'translateY(' + height + 'px)'
+        });
+        if (height >= 480) {
+            clearInterval(cardboardBoxFall);
+        }
+    }, 100);
+}());
