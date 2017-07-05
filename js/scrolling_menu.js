23e2cfe582421899b9c7edf658475637b8c23c2d
dceynowa <dejw.ce@gmail.com>
2017-07-05 21:27:20 +0200

    Merge branch 'develop' of https://github.com/infoshareacademy/jfdz5-4apple into FZ54A-19-active-class-in-menu-DC and add class '.active' to last element in menu # Conflicts: #	index.html #	style.css

--- js/scrolling_menu.js
+++ js/scrolling_menu.js
@@ -1,35 +1,41 @@
-// Cache selectors
 var lastIdInMenuItems,
     menuList = $(".menu-list"),
-    menuHeight = menuList.outerHeight()+63,
+    menuHeight = menuList.outerHeight() + 63,
     // All list items
     menuItems = menuList.find("a"),
     // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
+    scrollItems = menuItems.map(function () {
         var item = $($(this).attr("href"));
-        if (item.length) { return item; }
+        if (item.length) {
+            return item;
+        }
     });
 
 
-// Bind to scroll
-$(window).scroll(function(){
+$(window).scroll(function () {
     // Get container scroll position
-    var fromTopMenu = $(this).scrollTop()+menuHeight;
+    var fromTopMenu = $(this).scrollTop() + menuHeight;
 
     // Get id of current scroll item
-    var cur = scrollItems.map(function(){
+    var cur = scrollItems.map(function () {
         if ($(this).offset().top < fromTopMenu)
             return this;
     });
     // Get the id of the current element
-    cur = cur[cur.length-1];
+    cur = cur[cur.length - 1];
     var id = cur && cur.length ? cur[0].id : "";
 
-    if (lastIdInMenuItems !== id) {
+    //set active class if you are on bottom page
+    if ($(window).scrollTop() + $(window).height() >= $(document).height() - $('footer').height()) {
+        $('li.active').removeClass('active');
+        $('.last-active-item').addClass('active');
+    }
+    else {
         lastIdInMenuItems = id;
         // Set/remove active class
         menuItems
             .parent().removeClass("active")
-            .end().filter("[href='#"+id+"']").parent().addClass("active");
+            .end().filter("[href='#" + id + "']").parent().addClass("active");
     }
+
 });
