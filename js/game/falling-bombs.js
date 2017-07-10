64b68908a840fdaf3db9c7f3880af3af2a477e12
dceynowa <dejw.ce@gmail.com>
2017-07-10 21:27:16 +0200

    FZ54A-22 random start bomb position

--- js/game/falling-bombs.js
+++ js/game/falling-bombs.js
@@ -1,17 +1,22 @@
+var boardWidth = $('.board').width();
+var $bomb = $('.bomb');
+var heightBomb = -15;
+var fallingTime = 100;
+// positive or negative startBombPosition
+var startBombPosition = Math.floor(Math.random($bomb.position().left + $bomb.width() / 2) * boardWidth / 2);
+startBombPosition *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+console.log(startBombPosition);
+
 function fallingBombs() {
-    var boardWidth = $('.board').width();
-    var $bomb = $('.bomb');
-    var heightBomb = 0;
-    var fallingTime = 100;
-setInterval(function(){
-    var bombPosition = $bomb.position().left + $bomb.width() / 2;
-    heightBomb += 10;
-    $('.bomb').css({
-        'transform': 'translateY(' + height + 'px)'
-    });
-    if (heightBomb >= 560) {
-        clearInterval(fallingTime)
-    }
-}, fallingTime)
+    setInterval(function () {
+        heightBomb += 10;
+        startBombPosition += 0
+        $('.bomb').css({
+            'transform': 'translate(' + startBombPosition + 'px, ' + height + 'px)'
+        });
+        if (heightBomb >= 560) {
+            clearInterval(fallingTime)
+        }
+    }, fallingTime)
 
 }
