4a27ebfee034d4a60e629f925ab02321ede95cd8
Piotr Stefanowicz <stefan8357@gmail.com>
2017-07-11 08:42:40 +0200

    FZ54A-22-game-difficulty-level/ small fixes

--- js/game/game.js
+++ js/game/game.js
@@ -4,29 +4,23 @@ var planePosition = 0;
 var roundTimeInSeconds = 60;
 var planesPathLength = $('.board').width() - $('.plane').width();
 var intervalTime = parseInt((roundTimeInSeconds / planesPathLength) * 1000);
-
+var $character = $('.character');
 
 $(window).keydown(function (e) {
-    $character = $('.character');
+
     if (e.keyCode === 37) {
         if (position > -255)
             position -= 15;
-
         $character.css({
-            'transform': 'translateX(' + position + 'px)',
-            'background': 'url("img/ludzik-z-workiem-lewo.png")',
-            'background-size': 'cover'
-        });
+            'transform': 'translateX(' + position + 'px)'
+        }).removeClass('character-right').addClass('character-left');
     }
     else if (e.keyCode === 39) {
         if (position < 255)
             position += 15;
-
         $character.css({
-            'transform': 'translateX(' + position + 'px)',
-            'background': 'url("img/ludzik-z-workiem-prawo.png")',
-            'background-size': 'cover'
-        });
+            'transform': 'translateX(' + position + 'px)'
+        }).removeClass('character-left').addClass('character-right');
     }
 });
 
@@ -44,21 +38,17 @@ var roundTiming = setInterval(function () {
     }
 }, intervalTime);
 var cardboardBoxFall = setInterval(function () {
-    var mario = $(".character");
-    var box = $('.cardboard-box');
+    var $box = $('.cardboard-box');
 
-    var marioPosition = mario.position().left + mario.width() / 2;
+    var marioPosition = $character.position().left + $character.width() / 2;
     var boxPosition = box.position().left + box.width() / 2;
     height += 10;
-    // console.log(height);
-    $('.cardboard-box').css({
+    $box.css({
         'transform': 'translateY(' + height + 'px)'
     });
 
-    console.log(boxPosition - marioPosition);
-
     if(height >= 480 && height <=540  && Math.abs( boxPosition - marioPosition) < 35){
-        $('.cardboard-box').hide();
+        $box.hide();
         clearInterval(cardboardBoxFall);
     }
     else if (height >=560 ){
