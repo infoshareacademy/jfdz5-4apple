a21bbee7575ce1ee2ea674f8c01cf4a051831615
dceynowa <dejw.ce@gmail.com>
2017-07-13 00:07:30 +0200

    FZ54A-22 falling bombs - fix fallings bomb

--- js/game/falling-bombs.js
+++ js/game/falling-bombs.js
@@ -1,50 +1,25 @@
 $(document).ready(function () {
-    fallingBombs(setStartBombPosition());
-});
-
-var boardWidth = $('.board').width();
-var $bomb = $('.bomb');
-var fallingTime = 100;
-
-//var delay = [1000, 2000, 3000, 4000, 5000];
-
-function setStartBombPosition() {
-    var startBombPosition = Math.floor(Math.random($bomb.position().left + $bomb.width() / 2) * boardWidth / 2);
-    startBombPosition *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
-    return startBombPosition;
-}
-// function createBombs() {
-//     $('.bombs-container').append('<div class="bomb"></div>');
-//     setStartBombPosition();
-// }
-
-bombs = [
-    {
-        bombPosition: setStartBombPosition(),
-        falling: fallingBombs(this.bombPosition)
-    },
-    {
-        bombPosition: setStartBombPosition(),
-        falling: fallingBombs(this.bombPosition)
-    }
-];
-
-
-
-function fallingBombs(position) {
-    var heightBomb = 0;
-    setInterval(function () {
-        heightBomb += 10;
-        $bomb.each(function () {
-            $(this).css({
-                'transform': 'translate(' + position + 'px, ' + height + 'px)'
-            });
-        });
-
-        if (heightBomb >= 560) {
-            clearInterval(fallingTime)
+    var boardWidth = $('.board').width();
+    var $bombs = $('.bomb');
+    var fallingTime = 100;
+    var bombs = $.map($bombs, function (element) {
+        return {
+            x: Math.floor(Math.random() * boardWidth) - boardWidth / 2,
+            y: 0,
+            element: element
         }
-    }, fallingTime)
-}
+    });
 
+    var intervalId = setInterval(function () {
+        bombs.forEach(function (bomb) {
+            bomb.y += 2;
+            $(bomb.element).css({
+                'transform': 'translate(' + bomb.x + 'px, ' + bomb.y + 'px)'
+            });
 
+            if (bomb.y >= 560) {
+                $(bomb.element).hide();
+            }
+        });
+    }, fallingTime);
+});
