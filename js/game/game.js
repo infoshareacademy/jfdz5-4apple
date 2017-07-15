f077555cb049a6330e2d7a890d2d9d37b2460064
Piotr Stefanowicz <stefan8357@gmail.com>
2017-07-15 03:35:56 +0200

    FZ54A-22-game-difficulty-level/ translating boxes to be fixed

--- js/game/game.js
+++ js/game/game.js
@@ -1,5 +1,5 @@
 var position = 0;
-// var height = 0;
+var height1 = 0;
 var planePosition = 0;
 var roundTimeInSeconds = 10;
 var planesPathLength = $('.board').width() - $('.plane').width();
@@ -53,7 +53,7 @@ var fallingObjects = setInterval(function () {
     }));
     indexClasses += 1;
 
-}, 2000);
+}, 1000);
 
 var cardboardBox = [];
 console.log('tabluca', cardboardBox);
@@ -64,30 +64,26 @@ for (var i = 1; i <= 25; i++) {
 }
 console.log('tabluca', cardboardBox);
 
-var height = [0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240];
+var height = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240];
 
 var cardboardBoxFall = setInterval(function () {
 
-    console.log('height all',height);
 
-    var $box = $('.cardboard-box');
-    var $boxIteration = $('.cardboardBox'+indexClasses+'');
+    console.log('lista boxów z klasami', $('.cardboard-box'));
+    console.log('sprawdzene', $('div.cardboardBox'+indexClasses+'cardboard-box'));
+    var $box = $('div.cardboardBox'+indexClasses+'cardboard-box');
     console.log(indexClasses);
-    console.log('boxiterationweq', $('.cardboardBox'+indexClasses+''));
-    console.log('boxiteration', $boxIteration);
     var marioPosition = $character.position().left + $character.width() / 2;
     var boxPosition = $box.position().left + $box.width() / 2;
-    height[indexClasses] = height[indexClasses] + 10;
-    console.log('height',height[indexClasses]);
-    console.log('element',$boxIteration);
-
-    $boxIteration.css({
-        'transform': 'translateY(' + height[indexClasses] + 'px)'
+    height1 += 10;
+    console.log($box[indexClasses]);
+    $box.css({
+        'transform': 'translateY(' + height1 + 'px)'
     });
     if (height >= 480 && height <= 540 && Math.abs(boxPosition - marioPosition) < 35) {
         $box.hide();
     }
-    indexClasses = indexClasses + 1;
+    console.log('lista boxów z klasami', $box);
 }, 1000);
 
 
