cdecbeff213c30e8227e4979c3a1fa0defd0b34d
Piotr Stefanowicz <stefan8357@gmail.com>
2017-07-15 23:49:34 +0200

    FZ54A-22-game-difficulty-level/ character movement

--- js/game/game-object-oriented.js
+++ js/game/game-object-oriented.js
@@ -1,3 +1,37 @@
 /**
  * Created by Piotrek on 15.07.2017.
  */
+var $character = $('.character');
+
+var character = {
+    reference: $character,
+    height: $character.height,
+    width: $character.width,
+    PositionX: $character.position().left,
+    PositionY: $character.position().top,
+    moveLeft: function () {
+        if (character.PositionX > 0) {
+            character.PositionX -= 15;
+            $character.css({
+                left: character.PositionX
+            }).removeClass('character-right').addClass('character-left');
+        }
+    },
+    moveRight: function () {
+        if (character.PositionX < 510) {
+            character.PositionX += 15;
+            $character.css({
+                left: character.PositionX
+            }).removeClass('character-left').addClass('character-right');
+        }
+    }
+};
+$(window).keydown(function (e) {
+
+    if (e.keyCode === 37) {
+        character.moveLeft();
+    }
+    else if (e.keyCode === 39) {
+        character.moveRight();
+    }
+});
