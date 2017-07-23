2180eab505fa5cb2e073b731d5928e2260574e4c
Piotr Stefanowicz <stefan8357@gmail.com>
2017-07-23 23:29:21 +0200

    FZ54A-22-game-FIXES/ convey responsibility for animation to css

--- js/game/game-menu.js
+++ js/game/game-menu.js
@@ -1,18 +1,8 @@
 (function () {
 
-    truckMovement = setInterval(function () {
-        $('.truck').animate({
-            bottom: 53 + 'px'
-        }, "slow").animate({
-            bottom: 50 + 'px'
-        }, "slow");
-    }, 100);
-
     $('#play').click(function () {
         $('.game-menu').animate({height: 'toggle'});
         setTimeout(function () {
-            clearInterval(truckMovement);
-            $('.truck').remove();
             $('.game').show();
             startGame();
         }, 500);
@@ -21,6 +11,5 @@
     $('#quit').click(function () {
         $('.game-container').fadeOut(1000)
     })
-
 }());
 
