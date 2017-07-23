d96772573ce87f4a430ddbcc9d8e24f46ad38889
sajen87 <bolsik@gmail.com>
2017-07-23 20:55:37 +0200

    game load on js

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ $(function () {
     });
 });
 
+
+// SMOOTH ANIMATED SCROLL ON SNEEK PEAK BUTTON //
 $("a, .button-top").on('click', function(event) {
 
 
@@ -32,4 +34,42 @@ $("a, .button-top").on('click', function(event) {
             window.location.hash = hash;
         });
     } // End if
+});
+
+////////////////////////////////////////////
+// $( "form" ).submit(function( event ) {
+//    {    alert("wywolana");
+//     }
+//
+//
+// });
+
+var helper = {
+    allowed: [111, 102, 110, 105],
+    stored: [],
+    checker: function(first, second) {
+        return first.toString() === second.toString() ? true : false;
+    },
+    push: function(letter) {
+        helper.stored.unshift(letter);
+        console.log(helper.stored)
+    },
+    action: function() {
+        console.log("test");
+    }
+};
+
+document.addEventListener("keypress", function(e) {
+    if (helper.stored.length > 3) {
+        helper.stored.pop();
+    }
+
+    var event = e || window.event,
+        keyCode = event.keyCode || event.charCode;
+
+    helper.push(keyCode);
+
+    if (helper.checker(helper.allowed, helper.stored)) {
+        helper.action();
+    }
 });
