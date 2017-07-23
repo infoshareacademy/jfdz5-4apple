0fa6270c3eab235df34476edf0d8986b457018a1
sajen87 <bolsik@gmail.com>
2017-07-23 19:11:04 +0200

    added smooth scrool from main button to bottom

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ $(function () {
         }
     });
 });
+
+$("a, .button-top").on('click', function(event) {
+
+
+    if (this.hash !== "") {
+
+        event.preventDefault();
+
+
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 1400, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+    } // End if
+});
