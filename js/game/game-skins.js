7385b7a6625aee506c9b669085a99026ff74c7f4
Piotr Stefanowicz <stefan8357@gmail.com>
2017-07-23 08:24:18 +0200

    FZ54A-22-game-SKINS/ entire skins window built by js

--- js/game/game-skins.js
+++ js/game/game-skins.js
@@ -1,10 +1,15 @@
 var skinChoice = function () {
+    $('.game-container').append($('<div>').addClass('skins--container').text('Select skin')
+        .append($('<div>').addClass('skins-tile'))
+        .append($('<div>').addClass('skin-buttons-container')
+            .append($('<button>').addClass('skins--button button__save').attr('id', 'skins-save').text('save'))
+            .append($('<button>').addClass('skins--button button__cancel').attr('id', 'skins-cancel').text('cancel'))));
+
     skins = [];
     var skinIndex = 0;
     for (var skinId = 1; skinId <= 12; skinId++) {
         skins.push('img/skins/ludzik-z-workiem-prawo-' + skinId + '.png')
     }
-
     skins.map(function (skin) {
         skinIndex += 1;
         $('.skins-tile').append($('<div>').addClass('skin--element').css({
@@ -13,8 +18,7 @@ var skinChoice = function () {
     });
     $('.button__cancel').click(function () {
         $('.skins--container').animate({
-            height:'toggle'
+            height: 'toggle'
         })
     })
 }();
-
