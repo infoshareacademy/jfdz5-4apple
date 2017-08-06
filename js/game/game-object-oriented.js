6a214eb180df4e7377be1942d5acda6f75538427
Piotr Stefanowicz <stefan8357@gmail.com>
2017-08-06 11:20:41 +0200

    DVELOP-GAME-WORKING/tutorial initial commit

--- js/game/game-object-oriented.js
+++ js/game/game-object-oriented.js
@@ -1,4 +1,4 @@
-var startGame = function () {
+var startGame = function (isTutorial) {
 
     sessionStorage.clear('pointsHighScore');
 
@@ -270,6 +270,12 @@ var startGame = function () {
     });
 
     startRound();
+
+    if (isTutorial === true) {
+        clearInterval(roundIntervalId);
+        clearInterval(boxSpawn);
+        clearInterval(ticker);
+    }
 
 };
 
